6f9c90cbb6f82d4ca357bc8ad3cd5e9780ebef37
Jason Leach <jlspursfan@gmail.com>
2019-07-12 10:41:57 -0700

    Can register for trips, more orgs added, browse buttons on Home

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
 
   def home
-    @user = User.new 
+    @user = User.new
   end
 
   def show
     @project = Project.find(params[:id])
+    flash[:project_id] = @project.id
   end
 
   def index
--- app/controllers/user_trips_controller.rb
+++ app/controllers/user_trips_controller.rb
@@ -1,22 +1,32 @@
 class UserTripsController < ApplicationController
 
-  before_action: set_trip, only [:edit, :update, :show, :destroy]
+  before_action :set_trip, only: [:edit, :update, :show, :destroy]
   before_action :authorize
+  skip_before_action :verify_authenticity_token
 
   def new
     @user_trip = UserTrip.new
+    flash.keep
+    #byebug
   end
 
   def create
-    @user_trip = UserTrip.new(trip_params)
-    redirect_to @user_trip
+
+    @user_trip = UserTrip.create(:user_id => current_user.id, :project_id => flash[:project_id], :review => "")
+    @project = @user_trip.project
+    #flash[:less] = @project.less_one_spot
+    flash[:proj_id] = @project.id
+    flash[:notice] = "Congratulations! You have registered for this project!"
+    redirect_to project_path(@project)
   end
 
   def edit
   end
 
   def update
-    render :show
+    @user_trip = UserTrip.update(trip_params)
+    flash[:notice] = "Thank you for leaving a review."
+    redirect_to users_path(current_user)
   end
 
   def show
@@ -32,7 +42,7 @@ class UserTripsController < ApplicationController
   private
 
   def trip_params
-    params.require(:user_trip).permit(:review)
+    params.require(:user_trip).permit(:user_id, :project_id, :review)
   end
 
   def set_trip
